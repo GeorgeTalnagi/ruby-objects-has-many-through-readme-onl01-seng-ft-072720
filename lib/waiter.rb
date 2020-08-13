f0487c3940e71acac09429968fea5d5de4301e41
George Talnagi <gtalnagi@yahoo.com>
2020-08-13 00:39:31 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
-
+ 
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(self, waiter, total, tip)
+  end
+  
+    def meals
+      Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+ 
+  def best_tipper 
+    Customer.all select do |person|
+      meal.person == self 
+    end 
+  end 
+ 
 end
